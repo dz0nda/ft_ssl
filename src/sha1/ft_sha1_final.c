59035c50635fa59d3f9d20a397df3c997a4dc665
dz0nda <dzonda@student.42.fr>
2019-10-14 04:07:00 +0200

    - update: sha1

--- src/sha1/ft_sha1_final.c
+++ src/sha1/ft_sha1_final.c
@@ -34,33 +34,3 @@ void				ft_sha1_final(t_sha1_ctx *ctx, unsigned char *md)
 	}
 	ft_putstr("\n");
 }
-
-// void				ft_sha1_final(t_sha1_ctx *ctx, unsigned char *md)
-// {
-// 	int				ijk[3];
-// 	char			s[12];
-// 	uint8_t			*p;
-// 	unsigned char	*pmd = md;
-
-// 	ijk[0] = -1;
-// 	while (++ijk[0] < 4)
-// 	{
-// 		ijk[1] = -1;
-// 		p = (uint8_t *)&ctx->hash[ijk[0]];
-// 		while (++ijk[1] < 4)
-// 		{
-// 			ijk[2] = -1;
-// 			ft_bzero(s, sizeof(s));
-// 			ft_itoa(p[ijk[1]], s, 16);
-// 			ft_putstr(s);
-// 			while (s[++ijk[2]] != '\0')
-// 			{
-// 				*pmd = s[ijk[2]];
-// 				++pmd;
-// 			}
-// 		}
-// 		ft_putstr("\n");
-
-// 	}
-// 	ft_putstr("\n");
-// }
