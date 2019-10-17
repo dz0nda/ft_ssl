0aaaaa302982e4e19d8787651308f725a0d267e6
dz0nda <dzonda@student.42.fr>
2019-10-17 07:15:32 +0200

    - update: ssl error

--- src/ssl/ft_ssl_error.c
+++ src/ssl/ft_ssl_error.c
@@ -1,2 +1,15 @@
 #include "ft_ssl.h"
 
+int		ft_ssl_error(const char *cmd)
+{
+	ft_putstr("ft_ssl: Error: '");
+	ft_putstr(cmd);
+	ft_putstr("' is an invalid command.");
+	return (EXIT_FAILURE);
+}
+
+int    ft_ssl_parse_flag_unknown(char c)
+{
+    printf("unknown option '-%c'\n", c);
+    return (EXIT_FAILURE);
+}
--- src/ssl/ft_ssl_parse.c
+++ src/ssl/ft_ssl_parse.c
@@ -1,20 +1,6 @@
 #include "ft_ssl.h"
 
-int		ft_ssl_error(const char *cmd)
-{
-	ft_putstr("ft_ssl: Error: '");
-	ft_putstr(cmd);
-	ft_putstr("' is an invalid command.");
-	return (EXIT_FAILURE);
-}
-
-int    ft_ssl_parse_flag_unknown(char c)
-{
-    printf("unknown option '-%c'\n", c);
-    return (EXIT_FAILURE);
-}
-
-int ft_ssl_parse_flag(char c)
+int    ft_ssl_parse_flag(char c)
 {
     const char *flags = "pqrs";
     size_t  nbflags;
