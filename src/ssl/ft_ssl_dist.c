a5d15b62a2d353952c0a09123183bc3476469f83
dz0nda <dzonda@student.42.fr>
2019-11-06 08:09:01 +0100

    - add: dgst sha384 && sha512

--- src/ssl/ft_ssl_dist.c
+++ src/ssl/ft_ssl_dist.c
@@ -6,7 +6,7 @@
 /*   By: dzonda <dzonda@student.le-101.fr>          +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/11/04 22:13:04 by dzonda       #+#   ##    ##    #+#       */
-/*   Updated: 2019/11/05 04:23:06 by dzonda      ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/11/06 07:10:29 by dzonda      ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -17,7 +17,7 @@ int		ft_ssl_dist_exec(int argc, char *argv[])
 {
 	const t_ftssl_dist_t dist[FTSSL_DIST_E] = {
 		{ FTSSL_HELP, { "help" }, ft_ssl_usage },
-		{ FTSSL_DGST, { "md5", "sha1", "sha256" }, ft_ssl_dgst },
+		{ FTSSL_DGST, { "md5", "sha1", "sha256", "sha384", "sha512" }, ft_ssl_dgst },
 		{ FTSSL_CIPHER, { "base64", "des" }, ft_ssl_cipher },
 		{ FTSSL_STDRD, { "dgst" }, ft_ssl_stdrd }
 	};
