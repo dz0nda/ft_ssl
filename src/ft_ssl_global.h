416a94a1accca347cc5054b9506d6ae555b666cf
dz0nda <dzonda@student.42.fr>
2019-11-10 21:17:02 +0100

    - fix: minor flag error

--- src/ft_ssl_global.h
+++ src/ft_ssl_global.h
@@ -1,7 +1,7 @@
 #ifndef FT_SSL_GLOBAL_H
 # define FT_SSL_GLOBAL_H
 
-# include "../../lib/libft/libft.h"
+# include "../../libft/libft.h"
 # include <stdio.h>
 # include <sys/stat.h>
 # include <fcntl.h>
--- src/ssl/ft_ssl_error.c
+++ src/ssl/ft_ssl_error.c
@@ -6,7 +6,7 @@
 /*   By: dzonda <dzonda@student.le-101.fr>          +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/11/04 22:12:18 by dzonda       #+#   ##    ##    #+#       */
-/*   Updated: 2019/11/04 22:12:19 by dzonda      ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/11/10 21:04:47 by dzonda      ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -15,6 +15,7 @@
 
 int     ft_ssl_error(int argc, char *argv[])
 {
+    (void)argc;
     ft_putstr("ftssl:Error: '");
     ft_putstr(argv[0]);
     ft_putendl("' is an invalid command.");
