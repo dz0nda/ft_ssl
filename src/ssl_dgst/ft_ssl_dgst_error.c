beed4b7c68459e00f4e3863667a55125c7b7a87a
dz0nda <dzonda@student.42.fr>
2019-12-20 14:55:49 +0100

    - update: ssl_dgst errors refactor

--- src/ssl_dgst/ft_ssl_dgst_error.c
+++ src/ssl_dgst/ft_ssl_dgst_error.c
@@ -6,45 +6,30 @@
 /*   By: dzonda <dzonda@student.le-101.fr>          +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/11/05 04:10:49 by dzonda       #+#   ##    ##    #+#       */
-/*   Updated: 2019/11/13 14:38:35 by dzonda      ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/12/20 14:27:46 by dzonda      ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "ft_ssl_dgst.h"
 
-int     ft_ssl_dgst_error_opt(char *cmd, char *opt)
+int     ft_ssl_dgst_error(char *cmd, char *arg, int err)
 {
-    ft_putstr_fd(cmd, STDERR_FILENO);
-    ft_putstr_fd(": illegal option -- ", STDERR_FILENO);
-    ft_putendl_fd(opt, STDERR_FILENO);
-    ft_ssl_dgst_usage(cmd);
-    return (EXIT_FAILURE);
-}
+    const char *errors[FTSSL_DGST_ERR] = {
+        "Unexpected error.",
+        "illegal option -- ",
+        "option requires an argument -- ",
+        "No such file or directory"
+    };
 
-int     ft_ssl_dgst_error_opt_arg(char *cmd, char *opt)
-{
-    ft_putstr_fd(cmd, STDERR_FILENO);
-    ft_putstr_fd(": option requires an argument -- ", STDERR_FILENO);
-    ft_putendl_fd(opt, STDERR_FILENO);
-    ft_ssl_dgst_usage(cmd);
-    return (EXIT_FAILURE);
-}
-
-int     ft_ssl_dgst_error_file(char *cmd, char *arg)
-{
     ft_putstr_fd(cmd, STDERR_FILENO);
     ft_putstr_fd(": ", STDERR_FILENO);
+    if (err == FTSSL_DGST_ERR_OPT || err == FTSSL_DGST_ERR_ARG)
+        ft_putstr_fd(errors[err], STDERR_FILENO);
     ft_putstr_fd(arg, STDERR_FILENO);
-    ft_putendl_fd(": No such file or directory", 2);
-    return (EXIT_FAILURE);
-}
-
-int     ft_ssl_dgst_error_dir(char *cmd, char *arg)
-{
-    ft_putstr_fd(cmd, STDERR_FILENO);
-    ft_putstr_fd(": ", STDERR_FILENO);
-    ft_putstr_fd(arg, STDERR_FILENO);
-    ft_putendl_fd(": Is a directory", 2);
+    if (err == FTSSL_DGST_ERR_OPT || err == FTSSL_DGST_ERR_ARG)
+        ft_ssl_dgst_usage(cmd);
+    else
+        ft_putstr_fd(errors[err], STDERR_FILENO);
     return (EXIT_FAILURE);
 }
