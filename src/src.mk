5d2e1fbd1a3142c3d96a4ee3c552060e141a2234
dz0nda <dzonda@student.42.fr>
2021-02-11 18:44:47 +0100

    update src.mk

--- src/src.mk
+++ src/src.mk
@@ -6,13 +6,12 @@
 #    By: dzonda <dzonda@student.42lyon.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/02/29 23:36:16 by dzonda            #+#    #+#              #
-#    Updated: 2021/01/20 15:25:57 by dzonda           ###   ########lyon.fr    #
+#    Updated: 2021/02/11 18:21:17 by dzonda           ###   ########lyon.fr    #
 #                                                                              #
 # **************************************************************************** #
 
 SUBDIR = \
 			crypto/dgst \
-			crypto/dgst/hash \
 			ssl \
 			ssl/ssl_core \
 			ssl/ssl_cipher \
@@ -25,17 +24,17 @@ SUBFILE = \
 			ssl/ssl_core/ft_ssl_help.c \
 			ssl/ssl_core/ft_ssl_shell.c \
 			\
-			crypto/dgst/ft_dgst.c \
-			crypto/dgst/hash/ft_md5_utils.c \
-			crypto/dgst/hash/ft_md5.c \
-			crypto/dgst/hash/ft_sha1.c \
-			crypto/dgst/hash/ft_sha256_utils.c \
-			crypto/dgst/hash/ft_sha224.c \
-			crypto/dgst/hash/ft_sha256.c \
-			crypto/dgst/hash/ft_sha512_utils.c \
-			crypto/dgst/hash/ft_sha384.c \
-			crypto/dgst/hash/ft_sha512.c \
-			crypto/dgst/hash/ft_sha512256.c \
+			crypto/dgst/ft_md5_utils.c \
+			crypto/dgst/ft_md5.c \
+			crypto/dgst/ft_sha1_utils.c \
+			crypto/dgst/ft_sha1.c \
+			crypto/dgst/ft_sha256_utils.c \
+			crypto/dgst/ft_sha224.c \
+			crypto/dgst/ft_sha256.c \
+			crypto/dgst/ft_sha512_utils.c \
+			crypto/dgst/ft_sha384.c \
+			crypto/dgst/ft_sha512.c \
+			crypto/dgst/ft_sha512256.c \
 			\
 			ssl/ssl_dgst/ft_ssl_dgst.c \
 			ssl/ssl_dgst/ft_ssl_dgst_dist.c \
