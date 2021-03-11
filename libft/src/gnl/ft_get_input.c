6d5e1c97a58a1eb4a10e70fbb237861997ae9da7
dz0nda <dzonda@student.42.fr>
2021-03-11 21:20:11 +0100

    fix: ft_get_input EXIT_FAILURE

--- libft/src/gnl/ft_get_input.c
+++ libft/src/gnl/ft_get_input.c
@@ -6,12 +6,12 @@
 /*   By: dzonda <dzonda@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/12 17:43:12 by dzonda            #+#    #+#             */
-/*   Updated: 2021/02/18 16:36:09 by dzonda           ###   ########lyon.fr   */
+/*   Updated: 2021/03/11 21:14:41 by dzonda           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_get_next_line.h"
-
+#include<stdio.h>
 int		ft_get_input(char *file, char **input)
 {
 	int		fd;
@@ -26,7 +26,7 @@ int		ft_get_input(char *file, char **input)
 	nbuf = 0;
 	length = 0;
 	if (file != NULL && (fd = open(file, O_RDONLY)) == -1)
-		return (EXIT_FAILURE);
+		return (0);
 	while ((nbuf = read(fd, buf, 4096)) > 0)
 	{
 		tmp = ft_strnew(length);
